e454f93e995edf6ddb1487efd0b6aefb50002f00
nile1 <ng9454@gmail.com>
2017-10-24 16:16:58 +0530

    Calendar

---
+++ Calender/js/events.js
@@ -0,0 +1,107 @@
+var events = {
+	"user_2":[
+		{
+			"title": "SLEEPING",
+			"day": "Monday"
+		},
+		{
+			"title": "STUDY",
+			"day": "Monday"
+		},
+		{
+			"title": "TIMEPASS",
+			"day": "Tuesday"
+		},
+		{
+			"title": "COMPUTER GAMES",
+			"day": "Thursday"
+		},
+		{
+			"title": "TUTION",
+			"day": "Monday"
+		},
+		{
+			"title": "OUTDOOR SPORTS",
+			"day": "Sunday"
+		},
+	],
+	"user_4":[
+		{
+			"title": "SLEEPING",
+			"day": "Tuesday"
+		},
+		{
+			"title": "STUDY",
+			"day": "Monday"
+		},
+		{
+			"title": "TIMEPASS",
+			"day": "Tuesday"
+		},
+		{
+			"title": "COMPUTER GAMES",
+			"day": "Tuesday"
+		},
+		{
+			"title": "TUTION",
+			"day": "Friday"
+		},
+		{
+			"title": "OUTDOOR SPORTS",
+			"day": "Sunday"
+		},
+	],
+	"user_1":[
+		{
+			"title": "SLEEPING",
+			"day": "Sunday"
+		},
+		{
+			"title": "STUDY",
+			"day": "Wednesday"
+		},
+		{
+			"title": "TIMEPASS",
+			"day": "Wednesday"
+		},
+		{
+			"title": "COMPUTER GAMES",
+			"day": "Saturday"
+		},
+		{
+			"title": "TUTION",
+			"day": "Wednesday"
+		},
+		{
+			"title": "OUTDOOR SPORTS",
+			"day": "Monday"
+		},
+	],
+	"user_3":[
+		{
+			"title": "SLEEPING",
+			"day": "Thursday"
+		},
+		{
+			"title": "STUDY",
+			"day": "Thursday	"
+		},
+		{
+			"title": "TIMEPASS",
+			"day": "Thursday"
+		},
+		{
+			"title": "COMPUTER GAMES",
+			"day": "Saturday"
+		},
+		{
+			"title": "TUTION",
+			"day": "Friday"
+		},
+		{
+			"title": "OUTDOOR SPORTS",
+			"day": "Sunday"
+		},
+	],
+	
+}
